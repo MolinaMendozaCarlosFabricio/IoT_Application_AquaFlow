3b8c042d784f90373ad794d248f0d67e44582ee6
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2025-07-15 16:04:06 -0600

    Agregando atributos privados, agregando método para cerrar conexión

--- src/messaging/Publisher_RabbitMQ.py
+++ src/messaging/Publisher_RabbitMQ.py
@@ -2,12 +2,15 @@ import pika
 
 class PublisherAMQP:
     def __init__(self, config):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.getAMQPURL()))
-        self.channel = self.connection.channel()
-        self.exchange = config.getExchange()
-        self.channel.exchange_declare(exchange=self.exchange, exchange_type='topic')
+        self.__connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.getAMQPURL()))
+        self.__channel = self.__connection.channel()
+        self.__exchange = config.getExchange()
+        self.__channel.exchange_declare(exchange=self.__exchange, exchange_type='topic')
     
     def publishMessage(self, routing_key, message):
         print(f"Publicando mensaje en el tópico {routing_key}: {message}")
-        self.channel.basic_publish(exchange=self.exchange, routing_key=routing_key, body=message)
+        self.__channel.basic_publish(exchange=self.__exchange, routing_key=routing_key, body=message)
         print("Mensaje publicado!")
+    
+    def closeConnection(self):
+        self.__connection.close()
