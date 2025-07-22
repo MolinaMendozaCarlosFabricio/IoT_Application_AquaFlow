12921f5424763061737ac19ca15243b49a5153eb
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2025-07-22 15:10:38 -0600

    Agregando logs

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ import threading
 
 try:
     configManager = Config()
+    print("Configuración Obtenida")
     dbManager = LocalDB()
+    print("Base de datos iniciada")
     amqpManager = PublisherAMQP(configManager)
+    print("Conexión AMQP establecida")
     digitalSensorManager = DigitalSensors()
+    print("Sensores Digitales inicializados")
     analogicSensorManager = AnalogicSensors()
+    print("Sensores analógicos inicializados")
     mainView = MainView()
+    print("Vista inicializada")
 except Exception as e:
     print("Error al iniciar dependencias:", e)
 
