83a3ce3230aaf1d67f464fc2c6c3568eadac250b
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2025-07-23 00:46:11 -0600

    Arreglando hilos de ejecución

--- main.py
+++ main.py
@@ -7,61 +7,62 @@ from config import Config
 from src.index import Loop
 import threading
 
-# Iniciar configuración con dotenv
-try:
-    configManager = Config()
-    print("Configuración Obtenida")
-except Exception as e:
-    print("Error al obtener configuración:", e)
-    configManager = None
+def main():
+    try:
+        configManager = Config()
+        print("Configuración Obtenida")
+    except Exception as e:
+        print("Error al obtener configuración:", e)
+        configManager = None
 
-# Iniciar base de datos local
-try:
-    dbManager = LocalDB()
-    print("Base de datos iniciada")
-except Exception as e:
-    print("Error al iniciar base de datos:", e)
-    dbManager = None
+    try:
+        dbManager = LocalDB()
+        print("Base de datos iniciada")
+    except Exception as e:
+        print("Error al iniciar base de datos:", e)
+        dbManager = None
 
-# Conectar con RabbitMQ
-try:
-    amqpManager = PublisherAMQP(configManager)
-    print("Conexión AMQP establecida")
-except Exception as e:
-    print("Error al conectarse con servidor AMQP:", e)
-    amqpManager = None
+    try:
+        amqpManager = PublisherAMQP(configManager)
+        print("Conexión AMQP establecida")
+    except Exception as e:
+        print("Error al conectarse con servidor AMQP:", e)
+        amqpManager = None
 
-# Conectar con sensores digitales
-try:
-    digitalSensorManager = DigitalSensors()
-    print("Sensores Digitales inicializados")
-except:
-    print("Error al conectar con los sensores digitales:", e)
-    digitalSensorManager = None
+    try:
+        digitalSensorManager = DigitalSensors()
+        print("Sensores Digitales inicializados")
+    except Exception as e:
+        print("Error al conectar con los sensores digitales:", e)
+        digitalSensorManager = None
 
-# Conectar con sensores analógicos
-try:
-    analogicSensorManager = AnalogicSensors()
-    print("Sensores analógicos inicializados")
-except Exception as e:
-    print("Error al conectar con los sensores analógicos:", e)
-    analogicSensorManager = None
+    try:
+        analogicSensorManager = AnalogicSensors()
+        print("Sensores analógicos inicializados")
+    except Exception as e:
+        print("Error al conectar con los sensores analógicos:", e)
+        analogicSensorManager = None
 
-# Iniciar vista principal
-try:
-    mainView = MainView()
-    print("Vista inicializada")
-except Exception as e:
-    print("Error al iniciar vista:", e)
+    try:
+        mainView = MainView()
+        print("Vista inicializada")
+    except Exception as e:
+        print("Error al iniciar vista:", e)
+        return
 
-runing = True
-hilo_ui = threading.Thread(target=mainView.mainloop)
-hilo_ui.daemon = True
-hilo_ui.start()
+    hilo_loop = threading.Thread(target=system_loop, args=(analogicSensorManager, digitalSensorManager, dbManager, amqpManager, mainView))
+    hilo_loop.daemon = True
+    hilo_loop.start()
 
-while runing:
-    Loop(analogicSensorManager, digitalSensorManager, dbManager, amqpManager, mainView)
-    runing = mainView.verifyRunning()
-# Cerrar las conexiones con dependencias al finalizar
-dbManager.closeDB()
-amqpManager.closeConnection()
+    mainView.mainloop()
+
+    dbManager.closeDB()
+    amqpManager.closeConnection()
+
+def system_loop(analogicSensorManager, digitalSensorManager, dbManager, amqpManager, mainView):
+    while runing:
+        Loop(analogicSensorManager, digitalSensorManager, dbManager, amqpManager, mainView)
+        runing = mainView.verifyRunning()
+
+if __name__ == "__main__":
+    main()
