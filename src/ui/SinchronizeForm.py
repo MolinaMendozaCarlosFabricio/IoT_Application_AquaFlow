eaf35d91adaeea9e50edab3cd38db9835648bce9
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2025-07-27 13:26:35 -0600

    Agregando apartado de actividades para el agua, terminando vista de sincronización de usuario

--- src/ui/SinchronizeForm.py
+++ src/ui/SinchronizeForm.py
@@ -1,10 +1,22 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import messagebox
+from src.services.SynchronizeUser import SynchronizeUser
 
-class SinchronizeForm(tk.Tk):
-    def __init__(self):
+class SinchronizeForm(tk.Toplevel):
+    def __init__(self, dbManager):
         super().__init__()
         self.title("Sinchronize User Form - AquaFlow")
         self.geometry("300x400")
 
-        
+        self.db_manager = dbManager
+
+        tk.Label(self, text="ID de usuario:").grid(row=2, column=0, padx=5, pady=5)
+        self.user_id = tk.Entry(self)
+        self.user_id.grid(row=2, column=1, padx=5, pady=5)
+
+        tk.Button(self, text="Enviar", command=self.send_uid).grid(row=3, column=0, columnspan=2, pady=10)
+
+    def send_uid(self):
+        SynchronizeUser(self.db_manager, self.user_id.get())
+        messagebox.showinfo("Sincronizado", f"Sincronizado con el usuari: {self.user_id.get()}")
+        self.destroy()
